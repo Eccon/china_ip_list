ef7619e85d69c51e18587e5526879bc2a4fab46b
Eccon <18576718+Eccon@users.noreply.github.com>
2020-07-06 10:28:46 +0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,14 @@
+{
+echo "/log info \"Start updating CN_IP_List!\""
+echo "/log info \"Start removing old list!\""
+echo "/ip firewall address-list remove [/ip firewall address-list find list=CN]"
+echo "/log info \"Remove finished!\""
+echo "/log info \"Start importing CN_IP_List!\""
+echo "/ip firewall address-list"
+echo "add list=CN address=192.168.0.0/16"
+nets=`cat ./china_ip_list.txt`
+for net in $nets ; do
+  echo "add list=CN address=$net "
+done
+echo "/log info \"Importing CN_IP_List finished!\""
+} > test.rsc 
